d39d9e8b7f79c6d0f89b100864cb92fcc97080e0
8to16 <ampelectrecuted@gmail.com>
2025-10-19 14:11:33 +0100

    feat(gui): add acceptCustom for allowing entering custom text/number into extension dropdowns

--- packages/vm/src/engine/runtime.js
+++ packages/vm/src/engine/runtime.js
@@ -1344,16 +1344,33 @@ class Runtime extends EventEmitter {
                 type: menuId,
                 inputsInline: true,
                 output: "String",
-                colour: categoryInfo.color1,
-                colourSecondary: categoryInfo.color2,
-                colourTertiary: categoryInfo.color3,
-                extensions: categoryInfo.globalExtensions,
+                ...(menuInfo.acceptCustom
+                    ? {
+                          extensions: ["colours_textfield"],
+                      }
+                    : {
+                          colour: categoryInfo.color1,
+                          colourSecondary: categoryInfo.color2,
+                          colourTertiary: categoryInfo.color3,
+                          extensions: categoryInfo.globalExtensions,
+                      }),
                 outputShape: menuInfo.acceptReporters
                     ? ScratchBlocksConstants.OUTPUT_SHAPE_ROUND
                     : ScratchBlocksConstants.OUTPUT_SHAPE_SQUARE,
                 args0: [
                     {
-                        type: "field_dropdown",
+                        type: menuInfo.acceptCustom
+                            ? (() => {
+                                  switch (menuInfo.acceptCustom) {
+                                      case "number":
+                                          return "field_numberdropdown";
+                                      case "text":
+                                          return "field_textdropdown";
+                                      default:
+                                          return "field_dropdown";
+                                  }
+                              })()
+                            : "field_dropdown",
                         name: menuName,
                         options: menuItems,
                     },
