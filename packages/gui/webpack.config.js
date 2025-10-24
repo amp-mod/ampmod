7984dbe04c44d199b4de742cbfc1606d626c9629
8to16 <ampelectrecuted@gmail.com>
2025-10-24 18:35:32 +0100

    refactor(gui): heavily refactor info pages

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -9,6 +9,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const { EsbuildPlugin } = require("esbuild-loader");
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
 
 // PostCss
 const autoprefixer = require("autoprefixer");
@@ -290,10 +291,10 @@ module.exports = [
             "addon-settings": "./src/playground/addon-settings.jsx",
             credits: "./src/playground/credits/credits.jsx",
             home: "./src/playground/home/home.jsx",
-            notfound: "./src/playground/not-found/not-found.jsx",
+            notfound: "./src/playground/not-found.js",
             newcompiler: "./src/playground/new-compiler/new-compiler.jsx",
             examples: "./src/playground/examples/examples.jsx",
-            headeronly: "./src/playground/header-only.jsx",
+            privacy: "./src/playground/privacy.jsx",
         },
         output: {
             path: path.resolve(__dirname, "build"),
@@ -309,9 +310,12 @@ module.exports = [
         },
         plugins: base.plugins.concat([
             new HtmlWebpackPlugin({
-                chunks: ["headeronly"],
-                template: "src/playground/privacy.ejs",
+                chunks: ["privacy"],
+                title: `Privacy Policy - ${APP_NAME}`,
+                template: "src/playground/simple.ejs",
                 filename: "privacy.html",
+                inject: "body",
+                skipSimpleAnalytics: true,
                 ...htmlWebpackPluginCommon,
             }),
             new HtmlWebpackPlugin({
@@ -401,8 +405,10 @@ module.exports = [
                 template: "src/playground/simple.ejs",
                 filename: "404.html",
                 title: `Not Found - ${APP_NAME}`,
+                inject: "body", // if you go to skbiditoilet/index.html it will load skibiditoilet/js/not-found.js but it should load just js/not-found.js. this fixes that
                 ...htmlWebpackPluginCommon,
             }),
+            new HtmlInlineScriptPlugin([/(.*)(notfound|privacy).js(.*)/]),
             new CopyWebpackPlugin({
                 patterns: [
                     {
