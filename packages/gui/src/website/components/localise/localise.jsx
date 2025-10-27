e1bce3e3a331d91cc2f9e884c0f3a57fad48ed7d
8to16 <ampelectrecuted@gmail.com>
2025-10-27 18:30:08 +0000

    fix(gui): fix es-419 handling logic for website i18n I am an idiot. /j

--- packages/gui/src/website/components/localise/localise.jsx
+++ packages/gui/src/website/components/localise/localise.jsx
@@ -13,10 +13,6 @@ const translations = {};
 translationContext.keys().forEach(key => {
     const locale = key.replace(/^\.\/(.*)\.json$/, "$1");
     translations[locale] = translationContext(key);
-    if (key == "es-419") {
-        // We reuse our European Spanish translations for American Spanish instead of separating them.
-        return translationContext("es");
-    }
 });
 
 const supportedLocales = Object.keys(translations);
@@ -53,7 +49,14 @@ const interpolate = (text, values) => {
 };
 
 const getTranslation = (id, values = {}) => {
-    const locale = detectLocale(supportedLocales);
+    // We reuse our European Spanish translations for American Spanish instead of separating them.
+    const locale = () => {
+        const detectLocaleResult = detectLocale(supportedLocales);
+        if (detectLocaleResult === "es-419") {
+            return "es";
+        }
+        return detectLocaleResult;
+    };
     const localeTranslations = translations[locale] || translations.en;
     let translationObject = localeTranslations[id];
     if (!translationObject) {
