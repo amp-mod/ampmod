952ec889c05784e6a837a93a1e6df5672dd6f317
8to16 <ampelectrecuted@gmail.com>
2025-11-11 18:51:47 +0000

    feat(vm): rename a block in electro_test

--- packages/vm/src/extensions/ampmod_electro_test/index.js
+++ packages/vm/src/extensions/ampmod_electro_test/index.js
@@ -66,9 +66,8 @@ class ElectroTestBlocks {
                 {
                     blockType: BlockType.COMMAND,
                     opcode: 'helpUrlExists',
-                    text: 'I have my own helpURI',
-                    helpURI: 'https://ampmod.miraheze.org/wiki/AmpMod_Wiki:UltiFools/If_()_then_()_else_()',
-                    tooltip: "so when you right click me and click 'Help', I will not go to example.com"
+                    text: 'I have my own tooltip',
+                    tooltip: 'I have proof'
                 },
                 {
                     blockType: BlockType.INLINE,
