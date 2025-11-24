c10da676b79fecc7852c27273e2e34d4397f2afc
8to16 <ampelectrecuted@gmail.com>
2025-11-24 19:05:08 +0000

    refactor(vm): convert to ESM most of this was done using cjstoesm with some small fixes afterward

---
+++ packages/vm/src/blocks/ampmod_arrays.js
@@ -0,0 +1,71 @@
+import Cast from '../util/cast.js';
+import Runtime from '../engine/runtime.js';
+class AmpModArraysBlocks {
+    runtime;
+    constructor (runtime) {
+        /**
+         * The runtime instantiating this block package.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+    }
+    getPrimitives () {
+        return {
+            arrays_empty_array: this.emptyArray,
+            arrays_item_of: this.itemOf,
+            arrays_item_no_of: this.itemNoOf,
+            arrays_contains: this.contains,
+            arrays_length: this.length,
+            arrays_in_front_of: this.addFront, // kept swapped
+            arrays_behind: this.addBack, // kept swapped
+            arrays_at: this.insertAt,
+            arrays_range: this.range,
+            arrays_delimited_to_array: this.delimitedToArray
+        };
+    }
+    emptyArray () {
+        return [];
+    }
+    itemOf (args) {
+        return Cast.toList(args.VALUE)[Cast.toNumber(args.INDEX) - 1] ?? '';
+    }
+    itemNoOf (args) {
+        const a = Cast.toList(args.ARRAY);
+        const i = a.findIndex(x => x == args.VALUE);
+        return i === -1 ? 0 : i + 1;
+    }
+    contains (args) {
+        return Cast.toList(args.ARRAY).some(x => x == args.VALUE);
+    }
+    length (args) {
+        return Cast.toList(args.VALUE).length;
+    }
+    // intentionally swapped behaviors
+    addFront (args) {
+        const a = Cast.toList(args.ARRAY);
+        return [...a, args.ITEM]; // adds to end
+    }
+    addBack (args) {
+        const a = Cast.toList(args.ARRAY);
+        return [args.ITEM, ...a]; // adds to start
+    }
+    insertAt (args) {
+        const a = [...Cast.toList(args.ARRAY)];
+        a.splice(Math.max(0, Cast.toNumber(args.INDEX) - 1), 0, args.ITEM);
+        return a;
+    }
+    range (args) {
+        const s = Cast.toNumber(args.START);
+        const e = Cast.toNumber(args.END);
+        const step = s <= e ? 1 : -1;
+        const r = [];
+        for (let i = s; step > 0 ? i <= e : i >= e; i += step) {
+            r.push(i);
+        }
+        return r;
+    }
+    delimitedToArray (args) {
+        return Cast.toString(args.TEXT).split(Cast.toString(args.DELIM));
+    }
+}
+export default AmpModArraysBlocks;
--- packages/vm/src/extensions/scratch3_music/manifest.js
+++ packages/vm/src/extensions/scratch3_music/manifest.js
@@ -1,63 +1,125 @@
-module.exports = {
-    'drums/1-snare.mp3': require('./assets/drums/1-snare.mp3'),
-    'drums/2-bass-drum.mp3': require('./assets/drums/2-bass-drum.mp3'),
-    'drums/3-side-stick.mp3': require('./assets/drums/3-side-stick.mp3'),
-    'drums/4-crash-cymbal.mp3': require('./assets/drums/4-crash-cymbal.mp3'),
-    'drums/5-open-hi-hat.mp3': require('./assets/drums/5-open-hi-hat.mp3'),
-    'drums/6-closed-hi-hat.mp3': require('./assets/drums/6-closed-hi-hat.mp3'),
-    'drums/7-tambourine.mp3': require('./assets/drums/7-tambourine.mp3'),
-    'drums/8-hand-clap.mp3': require('./assets/drums/8-hand-clap.mp3'),
-    'drums/9-claves.mp3': require('./assets/drums/9-claves.mp3'),
-    'drums/10-wood-block.mp3': require('./assets/drums/10-wood-block.mp3'),
-    'drums/11-cowbell.mp3': require('./assets/drums/11-cowbell.mp3'),
-    'drums/12-triangle.mp3': require('./assets/drums/12-triangle.mp3'),
-    'drums/13-bongo.mp3': require('./assets/drums/13-bongo.mp3'),
-    'drums/14-conga.mp3': require('./assets/drums/14-conga.mp3'),
-    'drums/15-cabasa.mp3': require('./assets/drums/15-cabasa.mp3'),
-    'drums/16-guiro.mp3': require('./assets/drums/16-guiro.mp3'),
-    'drums/17-vibraslap.mp3': require('./assets/drums/17-vibraslap.mp3'),
-    'drums/18-cuica.mp3': require('./assets/drums/18-cuica.mp3'),
-    'instruments/1-piano/24.mp3': require('./assets/instruments/1-piano/24.mp3'),
-    'instruments/1-piano/36.mp3': require('./assets/instruments/1-piano/36.mp3'),
-    'instruments/1-piano/48.mp3': require('./assets/instruments/1-piano/48.mp3'),
-    'instruments/1-piano/60.mp3': require('./assets/instruments/1-piano/60.mp3'),
-    'instruments/1-piano/72.mp3': require('./assets/instruments/1-piano/72.mp3'),
-    'instruments/1-piano/84.mp3': require('./assets/instruments/1-piano/84.mp3'),
-    'instruments/1-piano/96.mp3': require('./assets/instruments/1-piano/96.mp3'),
-    'instruments/1-piano/108.mp3': require('./assets/instruments/1-piano/108.mp3'),
-    'instruments/2-electric-piano/60.mp3': require('./assets/instruments/2-electric-piano/60.mp3'),
-    'instruments/3-organ/60.mp3': require('./assets/instruments/3-organ/60.mp3'),
-    'instruments/4-guitar/60.mp3': require('./assets/instruments/4-guitar/60.mp3'),
-    'instruments/5-electric-guitar/60.mp3': require('./assets/instruments/5-electric-guitar/60.mp3'),
-    'instruments/6-bass/36.mp3': require('./assets/instruments/6-bass/36.mp3'),
-    'instruments/6-bass/48.mp3': require('./assets/instruments/6-bass/48.mp3'),
-    'instruments/7-pizzicato/60.mp3': require('./assets/instruments/7-pizzicato/60.mp3'),
-    'instruments/8-cello/36.mp3': require('./assets/instruments/8-cello/36.mp3'),
-    'instruments/8-cello/48.mp3': require('./assets/instruments/8-cello/48.mp3'),
-    'instruments/8-cello/60.mp3': require('./assets/instruments/8-cello/60.mp3'),
-    'instruments/9-trombone/36.mp3': require('./assets/instruments/9-trombone/36.mp3'),
-    'instruments/9-trombone/48.mp3': require('./assets/instruments/9-trombone/48.mp3'),
-    'instruments/9-trombone/60.mp3': require('./assets/instruments/9-trombone/60.mp3'),
-    'instruments/10-clarinet/48.mp3': require('./assets/instruments/10-clarinet/48.mp3'),
-    'instruments/10-clarinet/60.mp3': require('./assets/instruments/10-clarinet/60.mp3'),
-    'instruments/11-saxophone/36.mp3': require('./assets/instruments/11-saxophone/36.mp3'),
-    'instruments/11-saxophone/60.mp3': require('./assets/instruments/11-saxophone/60.mp3'),
-    'instruments/11-saxophone/84.mp3': require('./assets/instruments/11-saxophone/84.mp3'),
-    'instruments/12-flute/60.mp3': require('./assets/instruments/12-flute/60.mp3'),
-    'instruments/12-flute/72.mp3': require('./assets/instruments/12-flute/72.mp3'),
-    'instruments/13-wooden-flute/60.mp3': require('./assets/instruments/13-wooden-flute/60.mp3'),
-    'instruments/13-wooden-flute/72.mp3': require('./assets/instruments/13-wooden-flute/72.mp3'),
-    'instruments/14-bassoon/36.mp3': require('./assets/instruments/14-bassoon/36.mp3'),
-    'instruments/14-bassoon/48.mp3': require('./assets/instruments/14-bassoon/48.mp3'),
-    'instruments/14-bassoon/60.mp3': require('./assets/instruments/14-bassoon/60.mp3'),
-    'instruments/15-choir/48.mp3': require('./assets/instruments/15-choir/48.mp3'),
-    'instruments/15-choir/60.mp3': require('./assets/instruments/15-choir/60.mp3'),
-    'instruments/15-choir/72.mp3': require('./assets/instruments/15-choir/72.mp3'),
-    'instruments/16-vibraphone/60.mp3': require('./assets/instruments/16-vibraphone/60.mp3'),
-    'instruments/16-vibraphone/72.mp3': require('./assets/instruments/16-vibraphone/72.mp3'),
-    'instruments/17-music-box/60.mp3': require('./assets/instruments/17-music-box/60.mp3'),
-    'instruments/18-steel-drum/60.mp3': require('./assets/instruments/18-steel-drum/60.mp3'),
-    'instruments/19-marimba/60.mp3': require('./assets/instruments/19-marimba/60.mp3'),
-    'instruments/20-synth-lead/60.mp3': require('./assets/instruments/20-synth-lead/60.mp3'),
-    'instruments/21-synth-pad/60.mp3': require('./assets/instruments/21-synth-pad/60.mp3')
+const drums1SnareMp3 = require('./assets/drums/1-snare.mp3');
+const drums2BassDrumMp3 = require('./assets/drums/2-bass-drum.mp3');
+const drums3SideStickMp3 = require('./assets/drums/3-side-stick.mp3');
+const drums4CrashCymbalMp3 = require('./assets/drums/4-crash-cymbal.mp3');
+const drums5OpenHiHatMp3 = require('./assets/drums/5-open-hi-hat.mp3');
+const drums6ClosedHiHatMp3 = require('./assets/drums/6-closed-hi-hat.mp3');
+const drums7TambourineMp3 = require('./assets/drums/7-tambourine.mp3');
+const drums8HandClapMp3 = require('./assets/drums/8-hand-clap.mp3');
+const drums9ClavesMp3 = require('./assets/drums/9-claves.mp3');
+const drums10WoodBlockMp3 = require('./assets/drums/10-wood-block.mp3');
+const drums11CowbellMp3 = require('./assets/drums/11-cowbell.mp3');
+const drums12TriangleMp3 = require('./assets/drums/12-triangle.mp3');
+const drums13BongoMp3 = require('./assets/drums/13-bongo.mp3');
+const drums14CongaMp3 = require('./assets/drums/14-conga.mp3');
+const drums15CabasaMp3 = require('./assets/drums/15-cabasa.mp3');
+const drums16GuiroMp3 = require('./assets/drums/16-guiro.mp3');
+const drums17VibraslapMp3 = require('./assets/drums/17-vibraslap.mp3');
+const drums18CuicaMp3 = require('./assets/drums/18-cuica.mp3');
+const instruments1Piano24Mp3 = require('./assets/instruments/1-piano/24.mp3');
+const instruments1Piano36Mp3 = require('./assets/instruments/1-piano/36.mp3');
+const instruments1Piano48Mp3 = require('./assets/instruments/1-piano/48.mp3');
+const instruments1Piano60Mp3 = require('./assets/instruments/1-piano/60.mp3');
+const instruments1Piano72Mp3 = require('./assets/instruments/1-piano/72.mp3');
+const instruments1Piano84Mp3 = require('./assets/instruments/1-piano/84.mp3');
+const instruments1Piano96Mp3 = require('./assets/instruments/1-piano/96.mp3');
+const instruments1Piano108Mp3 = require('./assets/instruments/1-piano/108.mp3');
+const instruments2ElectricPiano60Mp3 = require('./assets/instruments/2-electric-piano/60.mp3');
+const instruments3Organ60Mp3 = require('./assets/instruments/3-organ/60.mp3');
+const instruments4Guitar60Mp3 = require('./assets/instruments/4-guitar/60.mp3');
+const instruments5ElectricGuitar60Mp3 = require('./assets/instruments/5-electric-guitar/60.mp3');
+const instruments6Bass36Mp3 = require('./assets/instruments/6-bass/36.mp3');
+const instruments6Bass48Mp3 = require('./assets/instruments/6-bass/48.mp3');
+const instruments7Pizzicato60Mp3 = require('./assets/instruments/7-pizzicato/60.mp3');
+const instruments8Cello36Mp3 = require('./assets/instruments/8-cello/36.mp3');
+const instruments8Cello48Mp3 = require('./assets/instruments/8-cello/48.mp3');
+const instruments8Cello60Mp3 = require('./assets/instruments/8-cello/60.mp3');
+const instruments9Trombone36Mp3 = require('./assets/instruments/9-trombone/36.mp3');
+const instruments9Trombone48Mp3 = require('./assets/instruments/9-trombone/48.mp3');
+const instruments9Trombone60Mp3 = require('./assets/instruments/9-trombone/60.mp3');
+const instruments10Clarinet48Mp3 = require('./assets/instruments/10-clarinet/48.mp3');
+const instruments10Clarinet60Mp3 = require('./assets/instruments/10-clarinet/60.mp3');
+const instruments11Saxophone36Mp3 = require('./assets/instruments/11-saxophone/36.mp3');
+const instruments11Saxophone60Mp3 = require('./assets/instruments/11-saxophone/60.mp3');
+const instruments11Saxophone84Mp3 = require('./assets/instruments/11-saxophone/84.mp3');
+const instruments12Flute60Mp3 = require('./assets/instruments/12-flute/60.mp3');
+const instruments12Flute72Mp3 = require('./assets/instruments/12-flute/72.mp3');
+const instruments13WoodenFlute60Mp3 = require('./assets/instruments/13-wooden-flute/60.mp3');
+const instruments13WoodenFlute72Mp3 = require('./assets/instruments/13-wooden-flute/72.mp3');
+const instruments14Bassoon36Mp3 = require('./assets/instruments/14-bassoon/36.mp3');
+const instruments14Bassoon48Mp3 = require('./assets/instruments/14-bassoon/48.mp3');
+const instruments14Bassoon60Mp3 = require('./assets/instruments/14-bassoon/60.mp3');
+const instruments15Choir48Mp3 = require('./assets/instruments/15-choir/48.mp3');
+const instruments15Choir60Mp3 = require('./assets/instruments/15-choir/60.mp3');
+const instruments15Choir72Mp3 = require('./assets/instruments/15-choir/72.mp3');
+const instruments16Vibraphone60Mp3 = require('./assets/instruments/16-vibraphone/60.mp3');
+const instruments16Vibraphone72Mp3 = require('./assets/instruments/16-vibraphone/72.mp3');
+const instruments17MusicBox60Mp3 = require('./assets/instruments/17-music-box/60.mp3');
+const instruments18SteelDrum60Mp3 = require('./assets/instruments/18-steel-drum/60.mp3');
+const instruments19Marimba60Mp3 = require('./assets/instruments/19-marimba/60.mp3');
+const instruments20SynthLead60Mp3 = require('./assets/instruments/20-synth-lead/60.mp3');
+const instruments21SynthPad60Mp3 = require('./assets/instruments/21-synth-pad/60.mp3');
+
+export default {
+    'drums/1-snare.mp3': drums1SnareMp3,
+    'drums/2-bass-drum.mp3': drums2BassDrumMp3,
+    'drums/3-side-stick.mp3': drums3SideStickMp3,
+    'drums/4-crash-cymbal.mp3': drums4CrashCymbalMp3,
+    'drums/5-open-hi-hat.mp3': drums5OpenHiHatMp3,
+    'drums/6-closed-hi-hat.mp3': drums6ClosedHiHatMp3,
+    'drums/7-tambourine.mp3': drums7TambourineMp3,
+    'drums/8-hand-clap.mp3': drums8HandClapMp3,
+    'drums/9-claves.mp3': drums9ClavesMp3,
+    'drums/10-wood-block.mp3': drums10WoodBlockMp3,
+    'drums/11-cowbell.mp3': drums11CowbellMp3,
+    'drums/12-triangle.mp3': drums12TriangleMp3,
+    'drums/13-bongo.mp3': drums13BongoMp3,
+    'drums/14-conga.mp3': drums14CongaMp3,
+    'drums/15-cabasa.mp3': drums15CabasaMp3,
+    'drums/16-guiro.mp3': drums16GuiroMp3,
+    'drums/17-vibraslap.mp3': drums17VibraslapMp3,
+    'drums/18-cuica.mp3': drums18CuicaMp3,
+    'instruments/1-piano/24.mp3': instruments1Piano24Mp3,
+    'instruments/1-piano/36.mp3': instruments1Piano36Mp3,
+    'instruments/1-piano/48.mp3': instruments1Piano48Mp3,
+    'instruments/1-piano/60.mp3': instruments1Piano60Mp3,
+    'instruments/1-piano/72.mp3': instruments1Piano72Mp3,
+    'instruments/1-piano/84.mp3': instruments1Piano84Mp3,
+    'instruments/1-piano/96.mp3': instruments1Piano96Mp3,
+    'instruments/1-piano/108.mp3': instruments1Piano108Mp3,
+    'instruments/2-electric-piano/60.mp3': instruments2ElectricPiano60Mp3,
+    'instruments/3-organ/60.mp3': instruments3Organ60Mp3,
+    'instruments/4-guitar/60.mp3': instruments4Guitar60Mp3,
+    'instruments/5-electric-guitar/60.mp3': instruments5ElectricGuitar60Mp3,
+    'instruments/6-bass/36.mp3': instruments6Bass36Mp3,
+    'instruments/6-bass/48.mp3': instruments6Bass48Mp3,
+    'instruments/7-pizzicato/60.mp3': instruments7Pizzicato60Mp3,
+    'instruments/8-cello/36.mp3': instruments8Cello36Mp3,
+    'instruments/8-cello/48.mp3': instruments8Cello48Mp3,
+    'instruments/8-cello/60.mp3': instruments8Cello60Mp3,
+    'instruments/9-trombone/36.mp3': instruments9Trombone36Mp3,
+    'instruments/9-trombone/48.mp3': instruments9Trombone48Mp3,
+    'instruments/9-trombone/60.mp3': instruments9Trombone60Mp3,
+    'instruments/10-clarinet/48.mp3': instruments10Clarinet48Mp3,
+    'instruments/10-clarinet/60.mp3': instruments10Clarinet60Mp3,
+    'instruments/11-saxophone/36.mp3': instruments11Saxophone36Mp3,
+    'instruments/11-saxophone/60.mp3': instruments11Saxophone60Mp3,
+    'instruments/11-saxophone/84.mp3': instruments11Saxophone84Mp3,
+    'instruments/12-flute/60.mp3': instruments12Flute60Mp3,
+    'instruments/12-flute/72.mp3': instruments12Flute72Mp3,
+    'instruments/13-wooden-flute/60.mp3': instruments13WoodenFlute60Mp3,
+    'instruments/13-wooden-flute/72.mp3': instruments13WoodenFlute72Mp3,
+    'instruments/14-bassoon/36.mp3': instruments14Bassoon36Mp3,
+    'instruments/14-bassoon/48.mp3': instruments14Bassoon48Mp3,
+    'instruments/14-bassoon/60.mp3': instruments14Bassoon60Mp3,
+    'instruments/15-choir/48.mp3': instruments15Choir48Mp3,
+    'instruments/15-choir/60.mp3': instruments15Choir60Mp3,
+    'instruments/15-choir/72.mp3': instruments15Choir72Mp3,
+    'instruments/16-vibraphone/60.mp3': instruments16Vibraphone60Mp3,
+    'instruments/16-vibraphone/72.mp3': instruments16Vibraphone72Mp3,
+    'instruments/17-music-box/60.mp3': instruments17MusicBox60Mp3,
+    'instruments/18-steel-drum/60.mp3': instruments18SteelDrum60Mp3,
+    'instruments/19-marimba/60.mp3': instruments19Marimba60Mp3,
+    'instruments/20-synth-lead/60.mp3': instruments20SynthLead60Mp3,
+    'instruments/21-synth-pad/60.mp3': instruments21SynthPad60Mp3
 };
