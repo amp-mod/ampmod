cf76441e64d112cc1e16356de81be1abe05316f8
8to16 <ampelectrecuted@gmail.com>
2025-12-06 21:27:10 +0000

    fix(gui): Revert "refactor(gui): rewrite toggle-buttons to use radix" This is because the "hide stage and sprite pane" addon, while technically working, looks a bit confusing when selecting the option.
    This reverts commit 883dbdd4ee91a80504b334a502cc41453ee80114.

--- packages/gui/src/components/toggle-buttons/toggle-buttons.jsx
+++ packages/gui/src/components/toggle-buttons/toggle-buttons.jsx
@@ -4,44 +4,33 @@ import classNames from 'classnames';
 
 import TWRenderRecoloredImage from '../../lib/tw-recolor/render.jsx';
 import styles from './toggle-buttons.css';
-import { ToggleGroup } from 'radix-ui';
 
-const ToggleButtons = ({buttons, className, disabled}) => {
-    const selectedValue = buttons.findIndex(b => b.isSelected);
-
-    return (
-        <ToggleGroup.Root
-            type="single"
-            className={classNames(className, styles.row, {
-                [styles.disabled]: disabled
-            })}
-            value={selectedValue !== -1 ? String(selectedValue) : undefined}
-            onValueChange={value => {
-                if (value) {
-                    buttons[Number(value)]?.handleClick();
-                }
-            }}
-            disabled={disabled}
-        >
-            {buttons.map((button, index) => (
-                <ToggleGroup.Item
-                    key={`toggle-${index}`}
-                    className={styles.button}
-                    title={button.title}
-                    aria-label={button.title}
-                    value={String(index)}
-                >
-                    <TWRenderRecoloredImage
-                        src={button.icon}
-                        aria-hidden="true"
-                        className={button.iconClassName}
-                        draggable={false}
-                    />
-                </ToggleGroup.Item>
-            ))}
-        </ToggleGroup.Root>
-    );
-};
+const ToggleButtons = ({buttons, className, disabled}) => (
+    <div
+        className={classNames(className, styles.row, {
+            [styles.disabled]: disabled
+        })}
+    >
+        {buttons.map((button, index) => (
+            <button
+                key={`toggle-${index}`}
+                className={styles.button}
+                title={button.title}
+                aria-label={button.title}
+                aria-pressed={button.isSelected}
+                onClick={button.handleClick}
+                disabled={disabled}
+            >
+                <TWRenderRecoloredImage
+                    src={button.icon}
+                    aria-hidden="true"
+                    className={button.iconClassName}
+                    draggable={false}
+                />
+            </button>
+        ))}
+    </div>
+);
 
 ToggleButtons.propTypes = {
     buttons: PropTypes.arrayOf(
