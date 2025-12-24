1155aac4690ee5bedcc17ec02ea8ff058972b8a7
8to16 <ampelectrecuted@gmail.com>
2025-12-24 20:48:47 +0000

    feat(gui): remove support for sb1 and sb2 the conversion logic adds quite a bit of bloat. if you want sb2 files to run in ampmod, import them into tw first.

--- packages/gui/src/lib/sb-file-uploader-hoc.jsx
+++ packages/gui/src/lib/sb-file-uploader-hoc.jsx
@@ -178,7 +178,7 @@ const SBFileUploaderHOC = function (WrappedComponent) {
             if (!fileInputFilename) return '';
             // only parse title with valid scratch project extensions
             // (scratch/turbowarp: .sb, .sb2, and .sb3) (ampmod: apz)
-            const matches = fileInputFilename.match(/^(.*)\.(sb[23]|apz)?$/);
+            const matches = fileInputFilename.match(/^(.*)\.(sb3|apz)?$/);
             if (!matches) return '';
             return matches[1].substring(0, 100); // truncate project title to max 100 chars
         }
