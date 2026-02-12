a952490d3f41e5e0253fc9e4d71559b5085c5f03
actd <actd@noreply.codeberg.org>
2026-02-12 13:24:46 +0100

    Update packages/gui/src/lib/amp-fun-facts.ts

--- packages/gui/src/lib/amp-fun-facts.ts
+++ packages/gui/src/lib/amp-fun-facts.ts
@@ -12,7 +12,7 @@
 export const tips = [
     'Tip: You can put variables inside of boolean and array inputs.',
     "Remember: For vanilla Scratch compatibility, use Legacy Lists and avoid blocks you don't recognise.",
-    'The <a href="https://ampmod.codeberg.page/manual/?utm_context=ampmod_loader" target="_blank">AmpMod Manual</a> has information for new AmpMod users.',
+    'The <a href="https://ampmod.codeberg.page/manual/?utm_source=ampmod_loader" target="_blank">AmpMod Manual</a> has information for new AmpMod users.',
     'Tip: "if () was matched, then" blocks must be in a "test if () was matched" block.',
     'Tip: You can create custom reporters in AmpMod using the return block.',
     'Tip: You can create your own AmpMod extensions to add new powerful blocks using JavaScript!',
@@ -20,7 +20,7 @@ export const tips = [
 ];
 
 export const facts = [
-    'Did you know? There is an <a href="https://ampmod.miraheze.org/?utm_context=ampmod_loader" target="_blank" rel="noopener">AmpMod Wiki</a> where you can find information about AmpMod.',
+    'Did you know? There is an <a href="https://ampmod.miraheze.org/?utm_source=ampmod_loader" target="_blank" rel="noopener">AmpMod Wiki</a> where you can find information about AmpMod.',
     'Fun fact: AmpMod is 100% free software. You can contribute to the source code <a href="https://codeberg.org/ampmod/ampmod" rel="noopener" target="_blank">on Codeberg</a>.',
     'Fun fact: Over 750 people use AmpMod almost every month.',
     'Fun fact: AmpMod builds like this one are fully reproducible on your own system.',
